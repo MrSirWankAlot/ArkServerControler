9bb0b75f8722a5d856bba648807021cedd224e43
Marvin <40034452+MrSirWankAlot@users.noreply.github.com>
2021-09-18 13:15:06 +0200

    First Version Upload

---
+++ Model/Setting.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkServerConfigEditor.Model
+{
+    public class Setting : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private string settingName = string.Empty;
+
+        private string settingValue = string.Empty;
+
+        public string SettingValue
+        {
+            get => settingValue;
+
+            set
+            {
+                if (Equals(settingValue, value))
+                {
+                    return;
+                }
+
+                settingValue = value;
+                this.OnPropertyChanged(nameof(SettingValue));
+            }
+        }
+        public string SettingName
+        {
+            get => settingName;
+          
+            set
+            {
+                if (Equals(settingName,value))
+                {
+                    return;
+                }
+
+                settingName = value;
+                this.OnPropertyChanged(nameof(SettingName));
+            }
+        }
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Viewmodel/MainMindowViewmodel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ArkServerConfigEditor.Annotations;
+using ArkServerConfigEditor.Model;
+
+namespace ArkServerConfigEditor.Viewmodel
+{
+    public class MainMindowViewmodel : INotifyPropertyChanged
+    {
+        private readonly string firstRow;
+        public readonly string IniPath;
+        public MainMindowViewmodel()
+        {
+            List<string> t = null;
+            IniPath = @"C:\Steamcmd\steamapps\common\ARK Survival Evolved Dedicated Server\ShooterGame\Saved\Config\WindowsServer\Game.ini";
+            try
+            {
+                t = File.ReadAllLines(IniPath).ToList();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("ReadError");
+                throw;
+            }
+
+            foreach (string s in t)
+            {
+                if (!s.Contains("="))
+                {
+                    firstRow = s;
+                    continue;
+                }
+
+                List<string> strings = s.Split("=").ToList();
+                SettingList.Add(new Setting { SettingName = strings[0], SettingValue = strings[1] });
+            }
+        }
+
+        private ObservableCollection<Setting> settingList = new ObservableCollection<Setting>();
+
+        public ObservableCollection<Setting> SettingList
+        {
+            get => settingList;
+
+            set
+            {
+                if (Equals(settingList, value))
+                {
+                    return;
+                }
+
+                settingList = value;
+                this.OnPropertyChanged(nameof(settingList));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
